2daaa94cb503f01454624cbf3af003f1304309a2
Luan Neves <luan.sousa@remessaonline.com.br>
2022-03-15 22:03:31 -0300

    feature: adiciona logica na camada de serviço

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
+import config from './config.js';
 import server from './server.js';
 import { logger } from './util.js';
  
-server.listen(3000)
-.on('listening', () => logger.info('server on!!!!'))
+server.listen(config.port)
+.on('listening', () => logger.info(`server on at ${config.port}!!!!`))
